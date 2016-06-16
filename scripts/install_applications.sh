df24d4b403d1b53949ea76a8b2b450ae9cf2a4b6
Ryan Edwards-Crewe <github@ryanedwardscrewe.com>
2016-06-16 18:24:30 -0400

    Update script to auto-accept install of packages

--- scripts/install_applications.sh
+++ scripts/install_applications.sh
@@ -10,13 +10,13 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 # Install Oracle JRE and JDK
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -y oracle-java8-installer
 
 # Set up environment variables
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install -y oracle-java8-set-default
 
 # Check the version
 java -version
 
 # Insntall postgresql
-sudo apt-get install postgresql
+sudo apt-get install -y postgresql

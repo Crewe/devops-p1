96d056730dd4bccc83a43df1428810dc12e55acc
Ryan Edwards-Crewe <github@ryanedwardscrewe.com>
2016-06-17 08:38:42 -0400

    Update install script
    Only needed the '-y' on the PostgreSQL

--- scripts/install_applications.sh
+++ scripts/install_applications.sh
@@ -10,10 +10,10 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 # Install Oracle JRE and JDK
-sudo apt-get install -y oracle-java8-installer
+sudo apt-get install oracle-java8-installer
 
 # Set up environment variables
-sudo apt-get install -y oracle-java8-set-default
+sudo apt-get install oracle-java8-set-default
 
 # Check the version
 java -version
